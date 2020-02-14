f6e81c33152557fafdc82bfa7ddd652eeebb4f8f
Derek Montgomery <derek.montgomery@wsu.edu>
2020-02-13 21:44:53 -0800

    Changed for gap start and continue gap score. Scoring system changed and Struct added.

--- main.c
+++ main.c
@@ -8,7 +8,9 @@ int main(int argc, char **argv)
 {
     match = 1;
     mismatch = -2;
-    gap = -1;
+    gapCont = -1;
+    gapOpen = -5;
+
     printf("Run Global Align\n");
     globalAlign(TestString1, TestString1);
 }
@@ -16,10 +18,11 @@ int main(int argc, char **argv)
 int globalAlign(char *string1, char *string2)
 {
     int rows = strlen(string1), columns =strlen(string2), i, j;
+    int tempInsert = 0, tempDel = 0, tempSub = 0;
     //Allocate Table
-    int len = sizeof(int *) *rows + sizeof(int) * columns * rows;
-    int **table = (int**) malloc(len);
-    int *ptr = (int *)(table + rows);
+    int len = sizeof(DPCELL *) *rows + sizeof(DPCELL) * columns * rows;
+    DPCELL **table = (int**) malloc(len);
+    DPCELL *ptr = (DPCELL *)(table + rows);
     //Set Row pointers
     for(i = 0; i < rows; i++)
     {
@@ -29,28 +32,113 @@ int globalAlign(char *string1, char *string2)
     //Initialize Table row 0 scores
     for(i = 0; i < rows; i++)
     {
-        table[i][0] = i*gap;
+        table[i][0].score = 0;
+        table[i][0].insertion = 0;
+        table[i][0].deletion = 0;
+        table[i][0].substitution = 0;
+        printf("[ ");
     }
     //Initialize Table Column 0 scores
     for(j = 0; j < columns; j++)
     {
-        table[0][j] = j*gap;
+        table[j][0].score = 0;
+        table[j][0].insertion = 0;
+        table[j][0].deletion = 0;
+        table[j][0].substitution = 0;
     }
 
     for(i = 1; i < rows; i++)
     {
         for(j = 1; j < columns; j++){
-            //Set current cell to Max of scores
-            table[i][j] = Max((table[i-1][j-1] + scoreChar(string1[i-1],string2[j-1])),
-                            (table[i-1][j] + gap), 
-                            (table[i][j-1] + gap));
+            tempInsert = maxInsertion(table, i, j);
+            tempSub = maxSubstitution(table, i, j, string1[i], string2[j]);
+            tempDel = maxDeletion(table, i, j);
+            table[i][j].insertion = tempInsert;
+            table[i][j].deletion = tempDel;
+            table[i][j].substitution = tempSub;
+            table[i][j].score = Max(tempInsert, tempDel, tempSub);
+            printf("%d ", table[i][j].score);
+
         }
+        printf("\n");
     }
 
-    printf("Maximum Global Alignment Score: %d\n", table[i -1][j -1]);
+    printf("Maximum Global Alignment Score: %d\n", table[i -1][j -1].score);
+}
 
+int maxInsertion(DPCELL **table, int i, int j)
+{
+    int insertion, deletion, substitution;
+    if( i == 0 && (j-1) == 0){
+        return 0;
+    }
+    if((j-1) == 0){
+        //insertion is negative infinite
+        //substitution is negative infinite
+        return gapOpen + (i*gapCont);
+    }
+    if((i -1) == 0){
+        //substituion is negative infinite
+        //deletion is negative infinite
+        return (gapOpen + (j * gapCont));
+    }
+    else{
+     insertion = table[i][j-1].insertion;
+     substitution = table[i][j-1].substitution + gapOpen + gapCont;
+     deletion = table[i][j-1].deletion + gapOpen + gapCont;
+    return Max(insertion, substitution, deletion);
+    }
+     
+}
 
+int maxDeletion(DPCELL** table, int i, int j)
+{
+    int insertion, deletion, substitution;
+    if( (i-1) == 0 && j == 0){
+        return 0;
+    }
+    if(j == 0){
+        //insertion is negative infinite
+        //substitution is negative infinite
+        return gapOpen + (i*gapCont);
+    }
+    if((i -1) == 0){
+        //substituion is negative infinite
+        //deletion is negative infinite
+        return (gapOpen + (j * gapCont));
+    }
+    
+    deletion = table[i -1][j].deletion + gapCont;
+    substitution = table[i-1][j].substitution + gapOpen + gapCont;
+    insertion = table[i-1][j].insertion + gapCont + gapOpen;
 
+    return Max(deletion, substitution, insertion);
+}
+
+int maxSubstitution(DPCELL **table, int i, int j, char ai, char bj){
+
+    int insertion, deletion, substitution;
+    int matchScore = scoreChar(ai, bj);
+
+    if( (i-1) == 0 && (j-1) == 0){
+        return matchScore;
+    }
+    if((j-1) == 0){
+        //insertion is negative infinite
+        //substitution is negative infinite
+        return (gapOpen + (i*gapCont)) + matchScore;
+    }
+    if((i -1) == 0){
+        //substituion is negative infinite
+        //deletion is negative infinite
+        return (gapOpen + (j * gapCont)) + matchScore;
+    }
+
+    substitution = table[i-1][j-1].substitution;
+    deletion = table[i-1][j-1].deletion;
+    insertion = table[i-1][j-1].insertion;
+    int max = Max(insertion, deletion, substitution);
+    return (max + matchScore);
 
 }
 
--- project.h
+++ project.h
@@ -2,8 +2,21 @@
 #include <string.h>
 #include <stdlib.h>
 
-int match, mismatch, gap;
+int match, mismatch, gapOpen, gapCont;
+
+typedef struct dpcell{
+    int score;
+    int insertion;
+    int deletion;
+    int substitution;
+}DPCELL;
 
 int globalAlign(char *string1, char *string2);
 int scoreChar(char ai, char bj);
 int Max(int s1, int s2, int s3);
+int maxDeletion(DPCELL** table, int i, int j);
+int maxInsertion(DPCELL **table, int i, int j);
+int maxSubstitution(DPCELL **table, int i, int j, char ai, char bj);
+
+
+
