f58a2797d714170554822843c0f59985b2e52f86
Derek Montgomery <derek.montgomery@wsu.edu>
2020-02-14 19:55:48 -0800

    debug

--- main.c
+++ main.c
@@ -310,7 +310,7 @@ char *insertGap(char *str, int index) // string has memory available allocated i
     char *dest = str + index;
     char *buf = (char *)malloc(sizeof(char) *strlen(str)); // buffer to hold built string
     strcpy(strEndBuf,dest);
-    strncpy(buf,str,index + 1); // move first part of string to buf
+    strncpy(buf,str,index); // move first part of string to buf
     buf[index] = '-';// insert '-', now not null terminated string
     buf[index + 1] = '\0';
     printf("buf front: %s\n buf end: %s\n",buf, strEndBuf);
